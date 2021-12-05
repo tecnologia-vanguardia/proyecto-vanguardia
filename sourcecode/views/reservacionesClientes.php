cbea6bb6f44411207e57a0330b50c04041273348
AdrianaZazuetaC <49730413+AdrianaZazuetaC@users.noreply.github.com>
2021-12-04 21:09:22 -0800

    modificacio de librerias

--- sourcecode/views/reservacionesClientes.php
+++ sourcecode/views/reservacionesClientes.php
@@ -13,8 +13,6 @@
 			    <table class="table table-bordered" style = "margin-top:10px" id= "tabla-reservas"></table>
 		  </div>
         <?php include("footer.php"); ?>
-        <script src="../js/jquery.min.js"></script>
-        <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
         <script src="../assets/bootstrap/js/bootstrap-table.js"></script>
         <script src="../symplebook/jsonRpc.js"></script>
         <script src="../symplebook/sympleBook.js"></script>
