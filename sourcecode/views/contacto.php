ff90d13a489551ea796e99769e76b5df01802e42
oliver-quintero <oliver.quintero@uabc.edu.mx>
2021-12-05 16:35:00 -0800

    Forma de contacto
    Se agrego formato de contacto basado en bootstrap.

--- sourcecode/views/contacto.php
+++ sourcecode/views/contacto.php
@@ -13,22 +13,52 @@ session_start();
      <?php include("nav-bar.php"); ?>
     
   <!-- about us -->
-<div class="container-fluid  p-2 ">
-    <div class="container-fluid  bg-success text-success"><br></div>
-    <div class="container-fluid p-3  bg-dark" style="color: #ffaf2a"><br>
-     
-     <img src="../assets/images/almarLogo.png" class="mx-auto d-block img-thumbnail" alt="jurel" width="400" ><br><br>
-
-      <h6 align="center" style="font-size:15px">Teléfono: (646) 121-33-84</h6>
-      <h6 align="center" style="font-size:15px">E-mail: contacto@almar.site</h6><br>
-      <h6 align="center" style="font-size:15px">Página creada por Ocean Web & Desing</h6>
+  <br/>
+  <center>
+  <img src="../assets/images/almarLogo.png" class="mx-auto d-block img-thumbnail" alt="jurel" width="250" >
+    <p>Llena este formulario para ponerte en contacto con nostros:</p>
+  <form style="width: 50%">
+  <div class="form-group row">
+    <label for="nombre" class="col-4 col-form-label">Nombre:</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>&nbsp;
+          </div>
+        </div> 
+        <input id="nombre" name="nombre" type="text" class="form-control">
+      </div><br/>
     </div>
+  </div>
+  <div class="form-group row">
+    <label for="tel" class="col-4 col-form-label">Teléfono:</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-phone"></i>&nbsp;
+          </div>
+        </div> 
+        <input id="tel" name="tel" type="text" class="form-control">
+      </div><br/>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="comentario" class="col-4 col-form-label">Comentario:</label> 
+    <div class="col-8">
+      <textarea id="comentario" name="comentario" cols="40" rows="5" class="form-control"></textarea>
+    </div>
+  </div> <br/>
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+  </div>
+</form><br/>
+</center>
 
+<?php include("footer.php"); ?>
 
-
- <!-- Pie de pagina -->
-<div class="container-fluid  bg-success text-success"><br></div>
-   
-</div>
 </body>
 </html>
